501c535e8726994c29335fbe3a944685683b6a62
Bot Devloper <telegrambotmaking12@gmail.com>
2025-09-07 12:58:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# ⚠️ Keep your secrets safe here
+API_ID = 25935455   # apna API_ID
+API_HASH = "47cb45d81d6e23bfc68671ea78cc6f9c"   # apna API_HASH
+BOT_TOKEN = "8002626546:AAExIwwy-dRjZ8c6YGPaEIKwYUwrd3FvEJQ"   # apna Bot Token
